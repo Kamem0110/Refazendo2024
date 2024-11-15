dd4b9e98e999ce1fabada93b744c0e4149cfc0e7
Kamem0110 <pedro_giusti@outlook.com>
2024-11-15 14:35:33 -0300

    Atualizado o sorteio

---
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quandidade: ${quantidade}`);
+    alert(`Do númer: ${de}`);
+    alert(`Até o número: ${ate}`)
+}
