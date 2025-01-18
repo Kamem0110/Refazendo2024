9d31ad57726e24107f64701d49e7eee9b60a1051
Kamem0110 <pedro_giusti@outlook.com>
2025-01-18 11:30:03 -0300

    Atividade de python 2025

---
+++ oo-sabor-express/modelo/restaurante.py
@@ -0,0 +1,11 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+    
+restautante_praca = Restaurante()
+restaurante_pizza = Restaurante()
+
+restaurantes = [restautante_praca, restaurante_pizza]
+
+print(restaurantes)
