c4ea766bf1924f28299ca256d22b534db7c1e0f9
Kamem0110 <pedro_giusti@outlook.com>
2025-01-18 11:30:39 -0300

    Atividade de python 2025

--- oo-sabor-express/modelo/restaurante.py
+++ oo-sabor-express/modelo/restaurante.py
@@ -4,8 +4,11 @@ class Restaurante:
     ativo = False
     
 restautante_praca = Restaurante()
+restautante_praca.nome = 'Praça'
+restautante_praca.categoria = 'Gourmet'
+
 restaurante_pizza = Restaurante()
 
 restaurantes = [restautante_praca, restaurante_pizza]
 
-print(restaurantes)
+print(vars(restautante_praca))
